a520efd50f87e15bc24ddf53a72906929fa72399
Robyn Arnecke <robynarnecke@gmail.com>
2022-02-04 14:17:32 -0600

    Command line now prompts the user and writes to the file using some of the answers.

--- README.md
+++ README.md
@@ -1,1 +1,51 @@
-howdy
+# PokemonVSNature    
+    
+    ---
+    
+    ## Description     
+    
+    Choose a day in the weather forecast to generate a list of Pokemon that you can use to battle it out!
+
+    ---
+    
+    ## Table of Contents    
+    
+
+
+    --- 
+    
+    ## Installation    
+
+    The command to install dependencies is N/A.
+
+    --- 
+
+    ## Usage    
+    
+    Each contributer had their own branch that they worked on. 
+
+    --- 
+
+    ## License    
+
+    --- 
+
+    ## Contributing    
+
+    
+
+    ---
+
+    ## Tests    
+    
+    The command to run tests is N/A. 
+
+    --- 
+
+    ## Questions    
+
+    Github repository:   
+    github.com/ribbonanarchy   
+    Email:    
+    robynarnecke@gmail.com    
+    
--- index.js
+++ index.js
@@ -62,18 +62,84 @@ const promptUser = () => {
 }
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {
-    fs.writeFileSync('README.md', 
+// GIVEN a command-line application that accepts user input
+// WHEN I am prompted for information about my application repository
+// THEN a high-quality, professional README.md is generated with the title of my project and sections entitled Description, Table of Contents, Installation, Usage, License, Contributing, Tests, and Questions
+// WHEN I enter my project title
+// THEN this is displayed as the title of the README
+// WHEN I enter a description, installation instructions, usage information, contribution guidelines, and test instructions
+// THEN this information is added to the sections of the README entitled Description, Installation, Usage, Contributing, and Tests
+// WHEN I choose a license for my application from a list of options
+// THEN a badge for that license is added near the top of the README and a notice is added to the section of the README entitled License that explains which license the application is covered under
+// WHEN I enter my GitHub username
+// THEN this is added to the section of the README entitled Questions, with a link to my GitHub profile
+// WHEN I enter my email address
+// THEN this is added to the section of the README entitled Questions, with instructions on how to reach me with additional questions
+// WHEN I click on the links in the Table of Contents
+// THEN I am taken to the corresponding section of the README
+function generateMarkdown({project_name, description, install_cmd, need_to_know, how_to_contribute, test_cmd, github, email}) {
+    
+    return `# ${project_name}    
+    
+    ---
+    
+    ## Description     
+    
+    ${description}
 
-    `howdy`
+    ---
+    
+    ## Table of Contents    
+    
 
-    );
+
+    --- 
+    
+    ## Installation    
+
+    The command to install dependencies is ${install_cmd}.
+
+    --- 
+
+    ## Usage    
+    
+    ${need_to_know}
+
+    --- 
+
+    ## License    
+
+    --- 
+
+    ## Contributing    
+
+    ${how_to_contribute}
+
+    ---
+
+    ## Tests    
+    
+    The command to run tests is ${test_cmd}. 
+
+    --- 
+
+    ## Questions    
+
+    Github repository:   
+    github.com/${github}   
+    Email:    
+    ${email}    
+    `;
 }
 
-writeToFile();
-
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    promptUser()
+    // Use writeFileSync method to use promises instead of a callback function
+    .then((answers) => fs.writeFileSync('README.md', generateMarkdown(answers)))
+    .then(() => console.log('Successfully wrote to README'))
+    .catch((err) => console.error(err));
+}
 
 // Function call to initialize app
 init();
